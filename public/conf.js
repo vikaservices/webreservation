402da4a898dff3ab0f78f4ea3f6b3f9bd696eaac
Jukka Turunen <jukka.turunen@windowslive.com>
2016-11-21 07:04:28 +0200

    conf

--- public/conf.js
+++ public/conf.js
@@ -1,7 +1,9 @@
-//const UIServerUrl = "/api/";
+const UIServerUrl = "/api/";
 //const UIServerUrl = "http://vob.fi:4000/";
-const UIServerUrl = "http://localhost:3000/";
+//const UIServerUrl = "http://localhost:3000/";
 
 const GOOGLE_ANALYTICS_KEY="UA-34352812-5";
 
 const DEFAULT_SEARCH_GROUP=72;
+
+const PRE_RESERVATION_TIMEOUT=10000;
